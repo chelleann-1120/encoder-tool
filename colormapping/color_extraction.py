0f1d20b4fffb411e15e40b3b77efa2a788f6f820
rochelle-pedralba <167157054+chelleann-1120@users.noreply.github.com>
2024-08-19 14:24:39 +0800

    added color extraction method

--- colormapping/color_extraction.py
+++ colormapping/color_extraction.py
@@ -1,17 +1,36 @@
-import cv2
 import numpy as np
+import cv2
 from PIL import Image
 import matplotlib.pyplot as plt
-import pytesseract
-from region_detection import ROI
-import os
+from region_detection import RegionDetection
 
-class ColorExtractor(ROI):
+class ColorExtractor(RegionDetection):
   '''
   Extracts color in the given image contour
   '''
 
-  def __init__(self, image_path):
-    super().__init__(image_path)
+  def __init__(self, image_path, image_name):
+    super().__init__(image_path, image_name)
     self.image_path = image_path
     self.color_legend = self.detect_color_legend()
+
+  def extract_legend_color(self):
+    image = Image.open(self.image_path).convert('RGB')
+    image_array = np.array(image)
+
+    x, y, width, height = cv2.boundingRect(self.color_legend)
+    legend_roi = image_array[y:y+height, x:x+width]
+
+    unique_colors = []
+    for row in legend_roi:
+        for color in row:
+            if not np.array_equal(color, [0, 0, 0]) and not any(np.array_equal(color, c) for c in unique_colors):
+                unique_colors.append(color)
+
+    unique_colors = np.array(unique_colors)
+
+    plt.figure(figsize=(10, 2))
+    plt.imshow([unique_colors], aspect='auto')
+    plt.title(self.image_path)
+    plt.axis('off')
+    plt.show()
