6da088d80619789d8823dbaba006d4bc77386eae
rochelle-pedralba <167157054+chelleann-1120@users.noreply.github.com>
2024-09-16 22:01:40 +0800

    Added method for finding max length

--- colormapping/heatmap_processor.py
+++ colormapping/heatmap_processor.py
@@ -12,22 +12,34 @@ class HeatmapProcessor:
 
   def process(self):
     image_path = os.path.join(self.input_dir, self.image)
-      
+
     try:
       values = TextExtraction(image_path, self.image)
       clean_values = TextFormatter(values)
-      matrix_values = GridProcessor(clean_values, image_path, self.image)
-      print(matrix_values.extract_legend_color())
-      matrix_values = matrix_values.create_grid_matrix()
+      matrix_values = GridProcessor(clean_values, image_path, self.image).create_grid_matrix()
 
-      #For clearly displaying the contents in the array only
-      print('')
-      for items in matrix_values:
-          print(items)
+      x_values = list(map(lambda items: items[0], matrix_values))
+      y_values = list(map(lambda items: items[1], matrix_values))
+      legend_values = list(map(lambda items: items[2], matrix_values))
 
-      # Pre-processing the data for decoder
-      # (Add your pre-processing code here)
+      return len(x_values), len(y_values), len(legend_values)
 
     except Exception as e:
       print(f"Error processing image {self.image}: {e}")
+
+  def find_max_length(self, x_max_len, y_max_len, legend_max_len):
+
+    x_len, y_len, legend_len = self.process()
+
+    if x_len > x_max_len:
+      x_max_len = x_len
+    
+    if y_len > y_max_len:
+      y_max_len = y_len
+    
+    if legend_len > legend_max_len:
+      legend_max_len = legend_len
+
+    print(x_max_len, y_max_len, legend_max_len)
+    return x_max_len, y_max_len, legend_max_len
 
--- colormapping/main.py
+++ colormapping/main.py
@@ -7,12 +7,19 @@ class HeatmapEncoder:
   def __init__(self, input_dir):
     self.input_dir = input_dir
     self.images = self.sort_images()
-  
+    self.x_max_len = 0
+    self.y_max_len = 0
+    self.legend_max_len = 0
+
+
   def run_main(self):
     
-    for image in self.images[8:9]:
+    for image in self.images[8:]:
       processor = HeatmapProcessor(self.input_dir, image)
-      processor.process()
+      self.x_max_len, self.y_max_len, self.legend_max_len = processor.find_max_length(self.x_max_len, self.y_max_len, self.legend_max_len)
+
+    # Separate the for loop, only applicable in finding the max_length of x, y, and legend
+
 
   def sort_images(self):
     sorted_images = sorted(os.listdir(self.input_dir), key=lambda x: [ord(c) for c in x])
